67a6bedeb60af6e329b8e7188583395c7e3b8e9e
stdd0101 <stdd01@mail.ru>
2021-10-16 21:43:01 +0300

    docker images init

---
+++ src/main/java/com/example/filestorageapp/FilestorageAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.filestorageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilestorageAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilestorageAppApplication.class, args);
+    }
+
+}
