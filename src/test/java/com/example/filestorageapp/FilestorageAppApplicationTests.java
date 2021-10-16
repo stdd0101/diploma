4984f434a6d6faef9020a05ded4fa08a816bab90
stdd0101 <stdd01@mail.ru>
2021-10-16 21:43:01 +0300

    docker images init

---
+++ src/test/java/com/example/filestorageapp/FilestorageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filestorageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilestorageAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
