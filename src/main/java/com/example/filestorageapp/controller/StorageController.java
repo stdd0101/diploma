b50d1f250e48abb31f538d5ab0bbfe23f47f6476
stdd0101 <stdd01@mail.ru>
2021-11-13 23:51:35 +0300

    refactor controller

--- src/main/java/com/example/filestorageapp/controller/StorageController.java
+++ src/main/java/com/example/filestorageapp/controller/StorageController.java
@@ -1,7 +1,6 @@
 package com.example.filestorageapp.controller;
 
 import com.example.filestorageapp.service.StorageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,28 +11,31 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/file")
 public class StorageController {
 
-        @Autowired
-        private StorageService service;
+    private StorageService service;
 
-        @PostMapping("/upload")
-        public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
-            return new ResponseEntity<>(service.upload(file), HttpStatus.OK);
-        }
+    public StorageController(StorageService service) {
+        this.service = service;
+    }
 
-        @GetMapping("/download/{fileName}")
-        public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
-            byte[] data = service.download(fileName);
-            ByteArrayResource resource = new ByteArrayResource(data);
-            return ResponseEntity
-                    .ok()
-                    .contentLength(data.length)
-                    .header("Content-type", "application/octet-stream")
-                    .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
-                    .body(resource);
-        }
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.upload(file), HttpStatus.OK);
+    }
 
-        @DeleteMapping("/delete/{fileName}")
-        public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
-            return new ResponseEntity<>(service.delete(fileName), HttpStatus.OK);
-        }
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.download(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.delete(fileName), HttpStatus.OK);
+    }
 }
