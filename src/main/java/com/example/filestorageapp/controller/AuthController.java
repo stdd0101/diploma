e1a4c7a0e71bc0796db1655ab9e40688ed422475
stdd0101 <stdd01@mail.ru>
2021-12-05 23:45:48 +0300

    fix controller for auth

--- src/main/java/com/example/filestorageapp/controller/AuthController.java
+++ src/main/java/com/example/filestorageapp/controller/AuthController.java
@@ -8,7 +8,6 @@ import com.example.filestorageapp.service.JwtUserDetailsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -31,10 +30,10 @@ class AuthController {
         this.userDetailsService = userDetailsService;
     }
 
-    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
 
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        this.authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
         final UserDetails userDetails = userDetailsService
                 .loadUserByUsername(authenticationRequest.getUsername());
@@ -47,8 +46,8 @@ class AuthController {
     private void authenticate(String username, String password) throws Exception {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        } catch (DisabledException exception) {
-            throw new Exception("USER_DISABLED", exception);
+//        } catch (DisabledException exception) {
+//            throw new Exception("USER_DISABLED", exception);
         } catch (BadCredentialsException exception) {
             throw new Exception("INVALID_CREDENTIALS", exception);
         }
--- src/main/java/com/example/filestorageapp/repository/UserRepository.java
+++ src/main/java/com/example/filestorageapp/repository/UserRepository.java
@@ -2,12 +2,12 @@ package com.example.filestorageapp.repository;
 
 import com.example.filestorageapp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByUsername(String username);
 
     Optional<User> findByUsernameOrEmail(String username, String email);
 }
