08e672845f09a17e97b8bab90fa9eaee6080414a
stdd0101 <stdd01@mail.ru>
2021-12-05 23:52:24 +0300

    fix init db

--- src/main/java/com/example/filestorageapp/repository/UserRepository.java
+++ src/main/java/com/example/filestorageapp/repository/UserRepository.java
@@ -2,11 +2,9 @@ package com.example.filestorageapp.repository;
 
 import com.example.filestorageapp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsernameOrEmail(String username, String email);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO public.users (id, username, email, password) VALUES (default, 'Antonio Calztonio4', 'wtf.dev4@20minutemail.it', '$2y$10$gcXLpPKWjcyetvszduqV6eQHL3MFbzYyY0WaysStLyNl5huoeTuom');
-INSERT INTO public.users (id, username, email, password) VALUES (default, 'Anna Filler', 'av@sdin.eu', '$2y$10$3/6PexVLf6KFEAd2.SIkKeCHMVDEXiLseL0zKoLf2eWd1ximQ21I6');
-INSERT INTO public.users (id, username, email, password) VALUES (default, 'Antonio Dev', 'algo.dev@mail.ru', '$2y$10$6hJo2uWrv/RLralMErIGLeGfEAHyQPmcOFEigBWvlCP4inFhJvYEe');
-INSERT INTO public.users (id, username, email, password) VALUES (default, 'Kcenia', 'jsmth2020@mail.ru', '$2y$10$vJ2z.NO1qBhlvpuH93x68.W1WG3eVajyjthsOmCd6V5gk1TE0O5Ni');
-INSERT INTO public.users (id, username, email, password) VALUES (default, 'Iris Hurrison', 'i.hurrison_a@mail.ru','$2y$10$1TtqVTUZ3LxFOdplDd86k.c1KLyDBrnKFh3sM/cMvSPPjads2I8FO');
-INSERT INTO public.users (id, username, email, password) VALUES (default, 'Antonio Figonio', 'ant.dev@oor_os.com', '$2y$10$GcODmE92rRH.SIH.RtjNDeTc1nyb7HPlxne0YQBdH9dYezXN/DIEa');
-INSERT INTO public.users (id, username, email, password) VALUES (default, 'Semenova Daria', 'stdd01@gmail.com', '$2y$10$Cy4lMuzRKgeY/yePb405yOv6lIpuUGrzWw0sm5MY1IkUoXWDkTqQy');
+INSERT INTO public.users (id, username, email, password) VALUES (1, 'Antonio Calztonio4', 'wtf.dev4@20minutemail.it', '$2y$10$gcXLpPKWjcyetvszduqV6eQHL3MFbzYyY0WaysStLyNl5huoeTuom');
+INSERT INTO public.users (id, username, email, password) VALUES (2, 'Anna Filler', 'av@sdin.eu', '$2y$10$3/6PexVLf6KFEAd2.SIkKeCHMVDEXiLseL0zKoLf2eWd1ximQ21I6');
+INSERT INTO public.users (id, username, email, password) VALUES (3, 'Antonio Dev', 'algo.dev@mail.ru', '$2y$10$6hJo2uWrv/RLralMErIGLeGfEAHyQPmcOFEigBWvlCP4inFhJvYEe');
+INSERT INTO public.users (id, username, email, password) VALUES (4, 'Kcenia', 'jsmth2020@mail.ru', '$2y$10$vJ2z.NO1qBhlvpuH93x68.W1WG3eVajyjthsOmCd6V5gk1TE0O5Ni');
+INSERT INTO public.users (id, username, email, password) VALUES (5, 'Iris Hurrison', 'i.hurrison_a@mail.ru','$2y$10$1TtqVTUZ3LxFOdplDd86k.c1KLyDBrnKFh3sM/cMvSPPjads2I8FO');
+INSERT INTO public.users (id, username, email, password) VALUES (6, 'Antonio Figonio', 'ant.dev@oor_os.com', '$2y$10$GcODmE92rRH.SIH.RtjNDeTc1nyb7HPlxne0YQBdH9dYezXN/DIEa');
+INSERT INTO public.users (id, username, email, password) VALUES (7, 'Semenova Daria', 'stdd01@gmail.com', '$2y$10$Cy4lMuzRKgeY/yePb405yOv6lIpuUGrzWw0sm5MY1IkUoXWDkTqQy');
